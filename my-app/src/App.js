9f00330ebe5bcfa853097bcbeb85e2f21fef84e6
Vladislav <kaknokote@gmail.com>
2024-09-25 21:11:07 +0300

    create a project

---
+++ my-app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg'; //императивный
+import './App.css';
+
+export const App = () => {
+	const currentYear = new Date().getFullYear();
+
+	return (
+		//заканчивается императивный, начинается декларативный
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<p>{currentYear}</p>
+			</header>
+		</div> //заканчивается декларативный
+	);
+};
